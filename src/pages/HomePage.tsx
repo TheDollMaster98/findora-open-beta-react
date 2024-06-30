ce09e70b9030609d618eba6b5f75d9f6f895df81
Pietroncello <marcocello26@gmail.com>
2024-06-30 18:44:41 +0200

    responsive homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,17 +11,17 @@ const HomePage: FunctionComponent<HomePageType> = ({ className = "" }) => {
       style={{ background: "linear-gradient(90deg, #1ab58a, #0e8cad)" }}
     >
       <div className="flex flex-col items-center justify-center flex-grow">
-        <div className="flex flex-row items-center justify-center gap-[3.75rem] animate-fade-down animate-once animate-duration-1000 animate-ease-in-out">
+        <div className="flex flex-row items-center justify-center gap-[3.75rem] animate-fade-down animate-once animate-duration-1000 animate-ease-in-out md:gap-[0rem] sm:gap-[1rem]">
           <div className="flex flex-row items-center justify-center">
             <img
-              className="w-[11.25rem] h-[11.25rem]"
+              className="w-[11.25rem] h-[11.25rem] md:h-[8.75rem] sm:w-[5rem] ss:w-[3rem]"
               alt=""
               src="/vector.svg"
             />
           </div>
           <div className="flex flex-col items-center justify-center">
             <img
-              className="w-[42.188rem] h-[9.375rem]"
+              className="w-[42.188rem] h-[9.375rem] md:w-[37.5rem] ml:w-[32.5rem] sm:w-[18.75rem] ss:w-[16rem] "
               alt=""
               src="/group-136.svg"
             />
