d1319a3e1febe629c1eaccccf211ae7fb5ee3f34
Pietroncello <marcocello26@gmail.com>
2024-06-30 12:52:09 +0200

    responsive missione

--- src/pages/MissionePage.tsx
+++ src/pages/MissionePage.tsx
@@ -68,33 +68,41 @@ const MissionePage: FunctionComponent<MissionePageType> = ({
           </b>
         </div>
         {/* Contenuto padre */}
-        <div className="w-full">
-          <div className="self-stretch flex-1 flex flex-col items-start justify-start py-[1.875rem] pr-[3.75rem] pl-[1.875rem] gap-[1.875rem] text-center text-[2rem] border-[4px] border-solid border-nero-3-findora">
+        <div className="w-full flex flex-row">
+          {/* Spacer */}
+          <div className="hidden md:block w-[120px] md:bg-nero-3-findora sm:hidden"></div>
+
+          <div className="self-stretch flex-1 flex flex-col items-start justify-start py-[1.875rem] pr-[3.75rem] pl-[1.875rem] gap-[1.875rem] text-center text-[2rem] border-[4px] border-solid border-nero-3-findora md:pr-[1.875rem]">
             <div className="self-stretch flex flex-row items-start justify-start gap-[0.937rem] ">
+              {/* Numerino */}
               <div
                 className={`w-[4.688rem] relative font-medium flex items-center justify-center shrink-0 ${
                   inView
                     ? "animate-fade-right animate-once animate-duration-1000 animate-delay-500 animate-ease-out"
                     : ""
-                }`}
+                } md:hidden`}
               >
                 03.1
               </div>
+              {/* titolo interno */}
               <b
                 className={`flex-1 relative text-[2.25rem] text-bianco-1-findora text-left ${
                   inView
                     ? "animate-fade-right animate-once animate-duration-1000 animate-delay-500 animate-ease-out"
                     : ""
-                }`}
+                } `}
               >
                 La nostra mission
               </b>
             </div>
-            <div className="self-stretch flex flex-row items-start justify-start py-[0rem] pr-[3.75rem] pl-[7.5rem] text-left text-white">
+            {/* container citazione esterna */}
+            <div className="self-stretch flex flex-row items-start justify-start py-[0rem] pr-[3.75rem] pl-[7.5rem] text-left text-white md:pr-[0rem] md:pl-[1.875rem] sm:pl-[0rem]">
+              {/* container citazione interna */}
               <div
-                className="w-[37.5rem] flex flex-row items-start justify-start min-w-[22.5rem] max-w-[37.5rem]
+                className=" flex flex-row items-start justify-start max-w-[37.5rem]
              "
               >
+                {/* citazione intera */}
                 <b
                   className={`relative flex-1  ${
                     inView
@@ -102,14 +110,16 @@ const MissionePage: FunctionComponent<MissionePageType> = ({
                       : ""
                   }`}
                 >
-                  <p className="m-0">
+                  {/* citazione 1 */}
+                  <p className="m-0 sm:text-[1.75rem]">
                     “Sostenere la crescita e lo sviluppo di carriere e
                     competenze, ispirando la realizzazione dei sogni e la
                     prosperità di creativi in tutto il mondo, attraverso
                     l’utilizzo di nuove tecnologie e meccanismi di gamification.
                   </p>
                   <p className="m-0">&nbsp;</p>
-                  <p className="m-0">
+                  {/* citazione 2 */}
+                  <p className="m-0 sm:text-[1.75rem]">
                     Il tutto rivoluzionando il sistema della competizione,
                     premiazione e visibilità Basato un sistema di ranking
                     innovativo, con un’approccio easy-to-use”
