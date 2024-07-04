35322fac4523f8af40727f19b0d05ed5a0ca5c44
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-07-04 23:29:15 +0200

    cambio colore

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -218,7 +218,7 @@ const Form: FunctionComponent<FormType> = ({ className = "", onClose }) => {
             <img
               className="w-[30px] relative h-[30px]"
               alt="close"
-              src="./public/xbianco.svg"
+              src="./xverde.svg"
             />
           </div>
         </div>
