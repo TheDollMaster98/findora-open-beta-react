594b61deabe3242a6b0df24b6553b4d360c95816
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-07-04 23:19:22 +0200

    deploy tutorial readme

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -218,7 +218,7 @@ const Form: FunctionComponent<FormType> = ({ className = "", onClose }) => {
             <img
               className="w-[30px] relative h-[30px]"
               alt="close"
-              src="./public/xverde.svg"
+              src="./public/xbianco.svg"
             />
           </div>
         </div>
