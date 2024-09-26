7b267eb9ac9d0dfcdd50d22497985aeff46863c8
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-09-26 10:58:30 +0200

    update url

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,36 @@ import {
 } from "react-router-dom";
 import FindoraLandingPage from "./pages/FindoraLandingPage";
 
-const baseUrl =
-  process.env.NODE_ENV === "production" ? "/findora-open-beta-react" : "";
+// Usa un baseUrl vuoto in produzione poiché ora il sito si trova alla root
+const baseUrl = process.env.NODE_ENV === "production" ? "" : "";
 
 function App() {
   const action = useNavigationType();
   const location = useLocation();
   const pathname = location.pathname;
 
+  // Scroll to top quando si naviga tra le pagine
   useEffect(() => {
     if (action !== "POP") {
       window.scrollTo(0, 0);
     }
   }, [action, pathname]);
 
+  // Imposta dinamicamente il titolo della pagina e la meta description in italiano
   useEffect(() => {
     let title = "";
     let metaDescription = "";
 
     switch (pathname) {
       case "/":
-        title = "Findora Open Beta Local";
-        metaDescription = "Findora Open Beta Local Description";
-        break;
-      case "/findora-open-beta-react/":
         title = "Findora Open Beta";
-        metaDescription = "Findora Open Beta Description";
+        metaDescription =
+          "Benvenuti su Findora Open Beta, la piattaforma innovativa per il futuro della blockchain.";
+        break;
+      default:
+        title = "Pagina di Findora";
+        metaDescription =
+          "Scopri di più sulla piattaforma Findora e le sue funzionalità.";
         break;
     }
 
